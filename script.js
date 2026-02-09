936bacdcfdd0349188ceb0d7ebe227a393362fe5
PauloDev <otaviopaulo0405@gmail.com>
2026-02-09 09:32:14 -0300

    first Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode(){
+    const html = document.documentElement
+
+    // if(html.classList.contains("light")){
+    //     html.classList.remove("light")
+    // }else{
+    //     html.classList.add("light")
+    // } Todo esse codigo pode ser resumido em: 
+    html.classList.toggle("light")
+    
+}
+
+
